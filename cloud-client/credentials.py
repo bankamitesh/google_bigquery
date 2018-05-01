4084668476f72a1a58c2e40d6a0204b29b7bae63
Mitesh Banka <banka.mitesh@gmail.com>
2018-05-01 15:09:31 +0530

    Add files via upload

---
+++ cloud-client/credentials.py
@@ -0,0 +1,21 @@
+def implicit():
+    from google.cloud import storage
+
+    # If you don't specify credentials when constructing the client, the
+    # client library will look for credentials in the environment.
+    storage_client = storage.Client()
+
+    # Make an authenticated API request
+    buckets = list(storage_client.list_buckets())
+    print(buckets)
+def explicit():
+    from google.cloud import storage
+
+    # Explicitly use service account credentials by specifying the private key
+    # file.
+    storage_client = storage.Client.from_service_account_json(
+        'Documents/yuktix-67752e973e8e.json')
+
+    # Make an authenticated API request
+    buckets = list(storage_client.list_buckets())
+    print(buckets)
